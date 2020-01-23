d4c5da2021a5ced2359d5fb85e677bda8d0d56b4
haganc3 <haganc3@wwu.edu>
2020-01-23 22:07:57 +0000

    Updates HelloWorld.java with comments

--- lab1/HelloWorld.java
+++ lab1/HelloWorld.java
@@ -1,3 +1,9 @@
+/*
+Name: Colton Hagan
+Date: 1/23/19
+Class: CS241
+Project: Prints out "Hello World"
+*/
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
