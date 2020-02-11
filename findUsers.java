33d400621027405a5a2f91a9cf585a5536e773c2
Colton Someone <coltonmhagan@gmail.com>
2020-02-10 22:35:45 -0800

    mostly done couple more touch ups

--- findUsers.java
+++ findUsers.java
@@ -1,63 +1,95 @@
+/*
+Author: Colton hagan
+Class: CS 241
+Date: 2/1/2020
+Purpose: Takes in via command line arguements a file, source person, and target/targets.
+         Program reads in the file, performs dijkstra on the file, and 
+         outputs the found names that find the target range, or the distance of the given name.
+*/
+
+//for reading in the file
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+//for the array list
 import java.util.ArrayList;
-//import java.lang.Math;
 public class findUsers {
    //graph to be printed
    private static int[][] graph;
    //vertexes on graph
    private static ArrayList<String> vertexNames;
+   //number of vertices on ints
+   private static int numVertices;
    public static void main(String args[]) throws IOException {
       String sourcePerson = args[1];
-      BufferedReader fileReader = new BufferedReader(new FileReader(args[0]));
-      String currLine;
-      ArrayList<String> words = new ArrayList<String>();
-      while ((currLine = fileReader.readLine()) != null) {
-         String[] currWords = currLine.split(" ");
-         words.add(currWords[0]);
-         words.add(currWords[1]);
-         words.add(currWords[2]);
-      }
-      createVertex(words);
-      createGraph(words);
-      //printGraph();
-      int[] dijkstraDistances = Dijkstra(sourcePerson);
+      ArrayList<String> namesAndWeight = readFile(args[0]);
+      createVertex(namesAndWeight);
+      createGraph(namesAndWeight);
+      int[] dijkstraDistances = dijkstra(sourcePerson);
       findCorrectOutput(dijkstraDistances, sourcePerson, args[2]);
    }
-   public static void findCorrectOutput(int[] dijkstraDistances, String sourcePerson, String targetPerson) {
+   
+   //Takes in the name of a file and reads the file in and converts it into a array list
+   public static ArrayList<String> readFile(String fileName) throws IOException {
+      BufferedReader fileReader = new BufferedReader(new FileReader(fileName));
+      String currLine;
+      ArrayList<String> namesAndWeight = new ArrayList<String>();
+      //Reads the lines into a arrayList then outputs it
+      while ((currLine = fileReader.readLine()) != null) {
+         String[] currWords = currLine.split(" ");
+         namesAndWeight.add(currWords[0]);
+         namesAndWeight.add(currWords[1]);
+         namesAndWeight.add(currWords[2]);
+      }
+      fileReader.close();
+      return namesAndWeight;
+   }
+   
+   /*Takes in the found distances for the vertexs, the source person, and your target
+     (targetPerson, lt, gt, or eq) */
+   public static void findCorrectOutput(int[] dijkstraDistances, String sourcePerson,
+                                        String targetIdea) {
       //Saves into a arrayList to allow fenceposting
       ArrayList<String> output = new ArrayList<String>();
-      if(targetPerson.matches(".*eq\\d.*")) {
-         int targetNumber = Integer.parseInt(targetPerson.replaceAll("[^0-9]", ""));
-         for(int i = 0; i < dijkstraDistances.length; i++) {
-            if(dijkstraDistances[i] == targetNumber && i != getVertexID(sourcePerson, vertexNames)) {
+      String targetNumber = targetIdea.replaceAll("[^0-9]", "");
+      /*Uses series of if statements with redundency due to import rule not allowing BiPredicate
+        Saves if target idea is eq#*/
+      if(targetIdea.matches(".*eq\\d.*")) {
+         for(int i = 0; i < numVertices; i++) { 
+            if(dijkstraDistances[i] == Integer.parseInt(targetNumber) &&
+               //makes sure to not add self to the output output
+               i != getVertexID(sourcePerson)) {
                output.add(vertexNames.get(i));
             }
          }
-      } else if (targetPerson.matches(".*lt\\d.*")) {
-         int targetNumber = Integer.parseInt(targetPerson.replaceAll("[^0-9]", ""));
-         for(int i = 0; i < dijkstraDistances.length; i++) {
-            if(dijkstraDistances[i] < targetNumber && i != getVertexID(sourcePerson, vertexNames)) {
+      //Saves if target idea is lt#
+      } else if (targetIdea.matches(".*lt\\d.*")) {
+         for(int i = 0; i < numVertices; i++) {
+            if(dijkstraDistances[i] < Integer.parseInt(targetNumber) &&
+               i != getVertexID(sourcePerson)) {
                output.add(vertexNames.get(i));
             }
          }
-      } else if (targetPerson.matches(".*gt\\d.*")) {
-         int targetNumber = Integer.parseInt(targetPerson.replaceAll("[^0-9]", ""));
-         for(int i = 0; i < dijkstraDistances.length; i++) {
-            if(dijkstraDistances[i] > targetNumber && i != getVertexID(sourcePerson, vertexNames)) {
+      //Saves if target idea is gt#
+      } else if (targetIdea.matches(".*gt\\d.*")) {
+         for(int i = 0; i < numVertices; i++) {
+            if(dijkstraDistances[i] > Integer.parseInt(targetNumber) &&
+               i != getVertexID(sourcePerson)) {
                output.add(vertexNames.get(i));
             }
          }
+      //Saves if target idea is a name
       } else {
-         for(int i = 0; i < dijkstraDistances.length; i++) {
-            if(vertexNames.get(i).equals(targetPerson)) {
+         for(int i = 0; i < numVertices; i++) {
+            if(vertexNames.get(i).equals(targetIdea)) {
                output.add("" + dijkstraDistances[i]);
             }
          }
       }
       printOutput(output);
    }
+   
+   //Takes in a array list and outputs the contents, unles it is empty, it then prints none
    public static void printOutput(ArrayList<String> output) {
       if (output.size() != 0) {
          System.out.print(output.get(0));
@@ -65,25 +97,35 @@ public class findUsers {
             System.out.print(" " + output.get(i));
          }
       } else {
-         System.out.println("none");
+         System.out.print("none");
       }
    }
-   public static int[] Dijkstra(String sourcePerson) {
+   
+   //Takes in a person and finds the shortest path to all other names using dijkstra 
+   public static int[] dijkstra(String sourcePerson) {
       int[] distances = new int[vertexNames.size()];
-      for(int i = 0; i < distances.length; i++) {
+      /*Sets all the distances to infite/integer max value.
+        Uses number representation due to import guildlines */
+      for(int i = 0; i < numVertices; i++) {
          distances[i] = 2147483647;
       }
+      /*Creates array lists of all known and unknown names and then adds/subtracts
+        the source person from them*/
       ArrayList<String> known = new ArrayList<String>();
       known.add(sourcePerson);
       ArrayList<String> unknown = new ArrayList<String>(vertexNames);
       unknown.remove(sourcePerson);
-      distances[getVertexID(sourcePerson, vertexNames)] = 0;
+      distances[getVertexID(sourcePerson)] = 0;
       updateDistances(distances, sourcePerson, 0);
       int newPersonWeight = 0;
+      /*Runs dijkstra finding the shortest possible path based on current name until
+        there is no unkown names left or until there is only names with infinite values left*/
       while(!unknown.isEmpty() && newPersonWeight != 2147483647) {
+         //Uses the first name with infinite value in case of tie
          String newPerson = unknown.get(0);
          newPersonWeight = 2147483647;
-         for(int i = 0; i < vertexNames.size(); i++) {
+         //Finds the smallest distance left of the unknown names
+         for(int i = 0; i < numVertices; i++) {
             if(unknown.contains(vertexNames.get(i))) {
                if(distances[i] < newPersonWeight) {
                   newPersonWeight = distances[i];
@@ -91,61 +133,68 @@ public class findUsers {
                }
             }
          }
+         //Updates the known, unknown, and distances variables
          known.add(newPerson);
          unknown.remove(newPerson);
          updateDistances(distances, newPerson, newPersonWeight);
       }
       return distances;
    }
-  public static void updateDistances(int[] distances, String currentPerson, int prevousWeight) {
-      for(int i = 0; i < vertexNames.size(); i++) {
-         int graphWeight = graph[getVertexID(currentPerson, vertexNames)][i];
+   
+   /*Takes in the current shortest distances, current person, and the prevous weight
+     traversed to get to that person and updates the distance array with new values from that 
+     current person*/
+   public static void updateDistances(int[] distances, String currentPerson, int prevousWeight) {
+      for(int i = 0; i < numVertices; i++) {
+         //Current weight at that edge
+         int graphWeight = graph[getVertexID(currentPerson)][i];
          int weight = graphWeight + prevousWeight;
+         /*Sets distance equal to weight if and only if it is less then old value, 
+           not equal to zero (there is no edge), and if the old weight is not equal to infitity
+           (there are no edges that are touching it)*/
          if(weight < distances[i] && graphWeight != 0 && prevousWeight != 2147483647) {
             distances[i] = weight;
          }
       }
    }
-   public static void createVertex(ArrayList<String> words) {
+   
+   /*Is given a array list of names and weights and finds and filters all the names
+     into a non-repeating array list.*/
+   public static void createVertex(ArrayList<String> namesAndWeight) {
       vertexNames = new ArrayList<String>();
-      for(int i = 0; i < words.size(); i+=3) {
-         if(!vertexNames.contains(words.get(i))) {
-            vertexNames.add(words.get(i));
+      for(int i = 0; i < namesAndWeight.size(); i+=3) {
+         //Uses two if statements to skip third weight part of array list.
+         if(!vertexNames.contains(namesAndWeight.get(i))) {
+            vertexNames.add(namesAndWeight.get(i));
          }
-         if(!vertexNames.contains(words.get(i+1))) {
-            vertexNames.add(words.get(i+1));
+         if(!vertexNames.contains(namesAndWeight.get(i+1))) {
+            vertexNames.add(namesAndWeight.get(i+1));
          }
       }
+      //set variable of number of vertices
+      numVertices = vertexNames.size();
+
    }
-   public static void createGraph(ArrayList<String> array) {
+   
+   /*Takes in a array list of names and weights and creates a matrix representation of a directed,
+     weighted graph*/
+   public static void createGraph(ArrayList<String> namesAndWeight) {
       graph = new int[vertexNames.size()][vertexNames.size()];
-      for(int i = 0; i < array.size(); i += 3) {
-         graph[getVertexID(array.get(i), vertexNames)][getVertexID(array.get(i+1), vertexNames)] = Integer.parseInt(array.get(i+2));
+      for(int i = 0; i < namesAndWeight.size(); i += 3) {
+         //Saves weight into vararable to shorten lines
+         int weight = Integer.parseInt(namesAndWeight.get(i+2));
+         graph[getVertexID(namesAndWeight.get(i))][getVertexID(namesAndWeight.get(i+1))] = weight;
       }
    }
-   public static int getVertexID(String vertexName, ArrayList<String> array) {
-      for (int i = 0; i < array.size(); i++) {
-         if(vertexName.equals(array.get(i))) {
+   /*Takes in a vertex name and returns its ID in vertexNames array list,
+     returning -1 if there is no such name in the array list*/
+   public static int getVertexID(String vertexName) {
+      for (int i = 0; i < numVertices; i++) {
+         if(vertexName.equals(vertexNames.get(i))) {
             return i;
          }  
       }
+      //Returns if no match found
       return -1;
    }
-   private static void printGraph() {
-      int numVertices = vertexNames.size();
-      for(int i = 0; i < numVertices; i++) {
-            String spaces = "                   ";
-            //Uses substring to keep spacing consistant
-            spaces = spaces.substring(0, spaces.length() - vertexNames.get(i).length());
-            String output = vertexNames.get(i) + spaces;
-            System.out.print(output);
-            //Fenceposting
-            System.out.print("| " + graph[i][0] + " |");
-         for(int j = 1; j < numVertices; j++) {
-            System.out.print(" " + graph[i][j] + " |");
-         }
-         System.out.println("");
-      }
-   }
-
 }
