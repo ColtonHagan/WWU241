17a4193645d61b9d0fdda35f047aabf9dad6e6bc
Colton Someone <coltonmhagan@gmail.com>
2020-02-27 20:04:55 -0800

    lab5 finished

---
+++ lab5/Node.java
@@ -0,0 +1,40 @@
+/*
+  Name: Colton Hagan
+  Date: 2/13/20
+  Class: 241
+  Description: Node of BST tree
+*/
+public class Node{
+   private int value;
+   Node left, right;
+   public Node(int value){
+      this.value = value;
+   }
+   //Prints out value of current node
+   public int getValue(){
+      return value;
+   }
+   
+   public Node getLeftChild(){
+      return left;
+   }
+   
+   public Node getRightChild() {
+      return right;
+   }
+   
+   //Takes in value in inserts it on bst tree in the correct left/right position
+   public void insert(int value){
+      //Inserts left/right node
+      if (left == null && value <= getValue()){
+         left = new Node(value);
+      } else if (right == null && value >= getValue()) {
+         right = new Node(value);
+      //moves to the left/right to find place to insert node
+      } else if (value <= getValue()) {
+         left.insert(value);
+      } else {
+         right.insert(value);
+      }
+   }
+}
