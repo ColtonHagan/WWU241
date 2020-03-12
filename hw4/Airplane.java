b3bccc0eb4387b9012a40cdd9e2a565a99319d8d
Colton Someone <coltonmhagan@gmail.com>
2020-03-12 01:07:33 -0700

    ugly rough draft

--- hw4/Airplane.java
+++ hw4/Airplane.java
@@ -1,64 +1,108 @@
+/*
+Author: Colton Hagan
+Class: CS 241
+Date: 3/12/2020
+Purpose: Airplane object that keeps take of flight number, takeoff request time,
+         and number if passengers. Made for heap.
+*/
 import java.time.format.DateTimeFormatter;
 import java.time.LocalTime;
 public class Airplane {
    private String flightNum;
-   private String from;
-   private String to;
    private String time;
+   private int timeInMinutes;
    private int numPassengers;
-   private int inputPrio;
+   //If time has passed requested takeoff tiem
    private boolean requested;
-   public Airplane(String flightNum, String from, String to, String time, int numPassengers, int inputPrio) {
+   //Insializes values
+   public Airplane(String flightNum, String time, int numPassengers) {
       this.flightNum = flightNum;
-      this.from = from;
-      this.to = to;
       this.time = time;
       this.numPassengers = numPassengers;
-      this.inputPrio = inputPrio;
-      this.requested = false;
+      //Calculates time in minutes
+      timeInMinutes();
+      //At first no takeoff times have passed/ are up next
+      requested = false;
    }
+   //Converts and returns time in file to minutes
+   private void timeInMinutes() {
+      //If index 0, put to max value so it will be put down on the heap as low as possible
+      if(time == "filler") {
+         timeInMinutes = Integer.MAX_VALUE;
+      //Coverts time from hh:mm to just minutes
+      } else {
+         String fixedTime = hourFixer();
+         //fixes 12pm < 1pm problem
+         if(!fixedTime.substring(0,2).equals("12")) {
+            timeInMinutes += 720;
+         }
+         int inMinutes = Integer.parseInt(fixedTime.substring(0,2)) * 60 
+                         + Integer.parseInt(fixedTime.substring(3,5));
+         timeInMinutes += inMinutes;
+      }
+   }
+   
+   /*"fixes" time by adding a 0 in front if hour is 1 through 9 to allow localTime 
+     and timeInMinutes to correctly parses*/
+   private String hourFixer() {
+      String fixedTime = time;
+      if(time.length() == 4) {
+         fixedTime = "0" + fixedTime;
+      }
+      return fixedTime;
+   }
+   //Adds given time(in minutes) to requested takeoff time
+   public void addTime(int minutes) {
+      DateTimeFormatter timeFormator = DateTimeFormatter.ofPattern("hh:mm");
+      LocalTime updatedTime = LocalTime.parse(hourFixer());
+      String oldTime = time;
+      String newTime = timeFormator.format(updatedTime.plusMinutes(minutes));
+      //Deals with swapping from pm to am
+      if(oldTime.substring(0,2).equals("11") && !newTime.substring(0,2).equals("11")) {
+         timeInMinutes += 720;
+      }
+      time = newTime;
+      //Removes hourfixer fix
+      if(time.substring(0,1).equals("0")) {
+          time = time.substring(1,5);
+      }
+   }
+   //Prints out flight in diffrent ways, depending on a given way to do so
+   public void printFlight(int method) {
+      System.out.print(flightNum);
+      if(method == 2) {
+         System.out.print(" " + numPassengers);
+      } else if(method == 3) {
+         System.out.print(" departed at " + time);
+      }
+      System.out.println();
+   }
+   //Returns if the plane has passed its requested takeoff time
    public boolean getRequested() {
       return requested;
    }
+   //Changes if it has passed requested takeoff time
    public void changeRequestToTrue() {
       this.requested = true;
    }
+   //Returns flightNum of plane
    public String getFlightNum() {
       return flightNum;
    }
-   public String getFrom() {
-      return from;
+   //Returns time in requested takeoff time minutes of plane
+   public int getTimeInMinutes() {
+      return timeInMinutes;
    }
-   public String getTo() {
-      return to;
-   }
-   public void addTime(int minutes) {
-      DateTimeFormatter timeFormator = DateTimeFormatter.ofPattern("hh:mm");
-      LocalTime updatedTime = LocalTime.parse(time);
-      time = timeFormator.format(updatedTime.plusMinutes(minutes));
-   }
-   public int timeInMinutes() {
-      if(time == "filler") {
-         return Integer.MAX_VALUE;
-      }
-      int minutes = Integer.parseInt(time.substring(0,2)) * 60 + Integer.parseInt(time.substring(3,5));
-      int hour = Integer.parseInt(time.substring(0,2)) * 60;
-      return minutes;
-   }
+   //Returns requested takeoff time in hh:mm of plane
    public String getTime() {
       return time;
    }
-   
+   //Changes time to a given string
    public void changeTime(String newTime) {
       time = newTime;
    }
+   //Returns number of passengers on flight
    public int getNumPassengers() {
       return numPassengers;
-   }
-   public int getInputPrio() {
-      return inputPrio;
-   }
-   public void printFlight() {
-      System.out.println(flightNum + " " + from + " " + to + " " + time + " " + numPassengers);
    }
 }
--- hw4/TakeoffQueue.java
+++ hw4/TakeoffQueue.java
@@ -1,129 +1,168 @@
-//Node to self ints automatically convert to floor so no extra comand is needed
+/*
+Author: Colton Hagan
+Class: CS 241
+Date: 3/12/2020
+Purpose: Takes in a file of airplan takeoffs and a method/protocal number ranging from 1 to 3
+         reads file into a heap. Then orders the heap from max to min  based in the given method.
+         If method equals1 then orders and prints takeoffs based on order given in file. 
+         If method equals 2 then orders and prints takeoffs based on number of passenger on 
+         flight. If method equals 3 then orders and prints takeoffs based on time request
+         is made and number of passengers on flight.
+*/
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.FileReader;
 import java.util.ArrayList;
 public class TakeoffQueue {
+   //ArrayList heap
    private static ArrayList<Airplane> heap;
+   //Method used to sort
    private static int method;
    public static void main(String[] args) throws IOException { 
-      String arg[] = {"flightsA.txt", "3"};
-      method = Integer.parseInt(arg[1]);
+      method = Integer.parseInt(args[1]);
       heap = new ArrayList<Airplane>();
-      //filler value used for error reading
-      Airplane filler = new Airplane("filler", "filer", "filler", "filler", -1, -1);
+      //Filler value of index 0
+      Airplane filler = new Airplane("filler", "filler", -1);
       heap.add(filler);
-      readFile(arg[0]);
-      String lastTime = "12:59";
+      readFile(args[0]);
+      findAndPrintOrder();
+  
+   }
+   
+   /*Depending on the method/protocal orders the heap in the correct way
+     and then prints max value repeating into nothing is left in the heap*/
+   public static void findAndPrintOrder() {
+      //String and minute value of the last time ran
+      String lastTime = "";
       int lastTimeInMin = 0;
+      //Runs while heap has values in it
       while (heap.size() > 1) {
+         //if there are no times that have been requested, finds next requested time
          if(method == 3 && !heap.get(1).getRequested()) {
+            /*Temperally cahnges the method to 4 to sort by time to 
+            get the smallest time value to start on */
             method = 4;
             buildHeap();
+            //Resets to finding method by number of passengers
             method = 3;
          } 
-         buildHeap();
+         //Builds heap unless method=1, then keeps original order
+         if(method != 1) {
+            buildHeap();
+         }
+         //If it is the third method calculates the time for it
          if (method == 3) {
-            int timeToTakeoff = (int)Math.ceil((float)(heap.get(1).getNumPassengers()/2)/60);
-            if(lastTimeInMin > heap.get(1).timeInMinutes()) {
+            int timeToTakeoff = (int)Math.ceil(((double)heap.get(1).getNumPassengers()/2)/60);
+            //Finds what time the plan is taking off (scheduled or time it is delayed to)
+            if(lastTimeInMin > heap.get(1).getTimeInMinutes()) {
                heap.get(1).changeTime(lastTime);
             }
             heap.get(1).addTime(timeToTakeoff);
+            
+            //Notes all requested take off times that occur while current plane is taking off
             for(int i = 1; i < heap.size(); i++) {
-               if(heap.get(i).timeInMinutes() <= heap.get(1).timeInMinutes() && heap.get(i).getRequested() == false) {
+               if(heap.get(i).getTimeInMinutes() <= heap.get(1).getTimeInMinutes() &&
+                  !heap.get(i).getRequested()) {
                   heap.get(i).changeRequestToTrue();
                }
             }
          }
-         heap.get(1).printFlight();
+         heap.get(1).printFlight(method);
          lastTime = heap.get(1).getTime();
-         lastTimeInMin = heap.get(1).timeInMinutes();
-         removeRoot();
+         lastTimeInMin = heap.get(1).getTimeInMinutes();
+         //If it is the first method keeps order, else removes root and reorders
+         if (method == 1) {
+            heap.remove(1);
+         } else {
+            removeRoot();
+         }
       }
    }
-   
+   //Takes in a fileName and reads that file into a heap
    public static void readFile(String fileName) throws IOException {
       BufferedReader fileReader = new BufferedReader(new FileReader(fileName));
-      int inputPrio = Integer.MAX_VALUE;
+      //Is there a way with indexing to work out prio instead of using inputPrio
       String currLine;
       while((currLine = fileReader.readLine()) != null) {
          String[] words = currLine.split(" ");
-         Airplane currPlane = new Airplane(words[0], words[1], words[2], words[3], Integer.parseInt(words[4]), inputPrio);
+         Airplane currPlane = new Airplane(words[0], words[3], Integer.parseInt(words[4]));
          heap.add(currPlane);
-         inputPrio--;
       }
    }
+   //Takes in two indexes and swaps there place on the heap
    public static void swap(int index1, int index2) {
       Airplane temp = heap.get(index2);
       heap.set(index2, heap.get(index1));
       heap.set(index1, temp);
    }
+   //Takes in index and checks if it is in the heap, returning 0 if it isnt
    public static int exists(int index) {
-      if (index < heap.size()) {
+      if (index < heap.size() && index > -heap.size()) {
          return index;
       }
       return 0;
    }
-   
+   /*Takes in given index and currentValue and returns 0
+     if the request Time has not been passed, else returns passed value*/
+   public static int beenRequested (int index, int currentValue) {
+      if(heap.get(index).getRequested()) {
+         return currentValue;
+      } 
+      return 0;
+   }
+   /*Takes in an index of the heap and depending and the method/protocal called
+     evaluates if the parrent is bigger or smaller then its child, 
+     swaping and reruning if it is smaller*/
    public static void heapify(int index) {
-      //System.out.println(index);
-      int leftChild = getLeftChild(index);
-      int rightChild = getRightChild(index);
+      //left and right child index --> 0 if they don't exist
+      int leftChild = exists(getLeftChild(index));
+      int rightChild = exists(getRightChild(index));
+      index = exists(index);
       int leftChildValue = 0;
       int rightChildValue = 0;
       int maxValue = 0;
-      
-//this changes depending on mode
-      //try doing this via index position somehow...
-      if(method == 1) {
-         leftChildValue = heap.get(exists(leftChild)).getInputPrio();
-         rightChildValue = heap.get(exists(rightChild)).getInputPrio();
-         maxValue = heap.get(index).getInputPrio();
-      } else if (method == 2 || method == 3) {
-         leftChildValue = heap.get(exists(leftChild)).getNumPassengers();
-         rightChildValue = heap.get(exists(rightChild)).getNumPassengers();
+      //If the method is 2/3 sort by number of passengers
+      if (method == 2 || method == 3) {
+         leftChildValue = heap.get(leftChild).getNumPassengers();
+         rightChildValue = heap.get(rightChild).getNumPassengers();
          maxValue = heap.get(index).getNumPassengers();
+         //If the method 3 it makes all values who requestime hasnt been called 0
+         if(method == 3) {
+               leftChildValue = beenRequested(leftChild,leftChildValue);
+               rightChildValue = beenRequested(rightChild,rightChildValue);
+               maxValue = beenRequested(index,maxValue);
+         }
+      //If it is method 4 sorts by time
       } else if(method == 4) {
-         leftChildValue = -heap.get(exists(leftChild)).timeInMinutes();
-         rightChildValue = -heap.get(exists(rightChild)).timeInMinutes();
-         maxValue = -heap.get(index).timeInMinutes();
+         //Uses negetive values to make max heaping into min heaping
+         leftChildValue = -heap.get(leftChild).getTimeInMinutes();
+         rightChildValue = -heap.get(rightChild).getTimeInMinutes();
+         maxValue = -heap.get(index).getTimeInMinutes();
       }
-      if(method == 3) {
-         if(!heap.get(exists(leftChild)).getRequested()) {
-            leftChildValue = 0;
-         } 
-         if(!heap.get(exists(rightChild)).getRequested()) {
-            rightChildValue = 0;
-         }
-         if(!heap.get(index).getRequested()) {
-            maxValue = 0;
-         }
-      }
-//***
-      //System.out.println(heap.get(index).timeInMinutes());
-      //System.out.println(maxValue);
+      //Finds which of the 3 values is biggest
       int maxIndex = index;
-      if (leftChildValue > maxValue) {
-         maxValue = leftChildValue;
-         maxIndex = leftChild;
-      } 
       if (rightChildValue > maxValue) {
          maxValue = rightChildValue;
          maxIndex = rightChild;
       }
-      //System.out.println("maxIndex " + maxIndex);
-      //System.out.println("maxValue " + maxValue);
+      if (leftChildValue > maxValue) {
+         maxValue = leftChildValue;
+         maxIndex = leftChild;
+      } 
+      //if heap is smaller then one of its child values swap
       if(heap.get(index) != heap.get(maxIndex)) {
          swap(index, maxIndex);
          heapify(maxIndex);
       }
    }
+   //Bulds heap so highest value is at top
    public static void buildHeap() {
-      for(int i = (int)Math.floor(((double)heap.size() - 1)/2); i >= 1; i--) {
+      for(int i = (heap.size() - 1)/2; i >= 1; i--) {
          heapify(i);
       }
    }
    
+   //Removes the root (max value) and then sorts to find next biggest valalue
    public static void removeRoot() {
       heap.set(1, heap.get(heap.size()-1));
       heap.remove(heap.size()-1);
@@ -131,44 +170,16 @@ public class TakeoffQueue {
          heapify(1);
       }
    }
-   public static void insert(Airplane plane) {
-      heap.add(plane);
-      int pos = heap.size() - 1;
-      while(pos > 1) {
-         int parent = getParent(pos);
-         int currValue = heap.get(pos).getNumPassengers();
-         int parentValue = heap.get(parent).getNumPassengers();
-         //Move up and swaps if value > then parrent
-         if(currValue > parentValue) {
-            swap(parent, pos);
-            pos = getParent(pos);
-         //Uses as a way to break loop if it is a max-heap
-         } else {
-            pos = 1;
-         }
-      }
+   //Uses given index of heap to calculate and return position of parent
+   public static int getParent(int index) { 
+      return index/2;
    }
-   
-   public static void remove(Airplane plane) {
-   
+   //Uses give index of heap to calculate and return the positions of the left child
+   public static int getLeftChild(int index) { 
+      return index*2;
    }
-   //Uses given position to calculate and return position of parent
-   public static int getParent(int pos) { 
-      return (int)Math.floor(((double)pos)/2);
-   }
-   //Uses give position to calculate and return the positions of the left child
-   public static int getLeftChild(int pos) { 
-      return (int)Math.floor(((double)pos)*2);
-   }
-   //Uses give position to calculate and return the positions of the right child
-   public static int getRightChild(int pos) { 
-      return (int)Math.floor(((double)pos)*2+1);
-   }
-   //returns boolean if given position is pointing to a leaf
-   public static boolean isLeaf(int pos) { 
-      if(pos * 2 > heap.size()) {
-         return true;
-      } 
-      return false;
+   //Uses give index of heap to calculate and return the positions of the right child
+   public static int getRightChild(int index) { 
+      return index*2+1;
    }
 }
